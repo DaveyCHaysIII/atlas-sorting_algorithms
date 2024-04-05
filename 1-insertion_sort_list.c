b749d45e2a4eafb6c599380d322c5e9fd313500b
Davey <david.c.hays.iii@gmail.com>
2024-04-05 02:49:24 +0000

    didnt notice the print_array every swap

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list()- insertion sort!
+ * @list: the list to sort
+ *
+ * Return: no return
+ */
+
+void insertion_sort_list(listint_t **list);
+{
+
+}
