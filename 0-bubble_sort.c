43acbbf66f958289bac3e742a1ccd871358fbb6c
Davey <david.c.hays.iii@gmail.com>
2024-04-05 02:22:10 +0000

    fixed var name

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -17,11 +17,11 @@ void bubble_sort(int *array, size_t size)
 	{
 		for (j = 0; j < size - i - 1; j++)
 		{
-			if (arr[j] > arr[j + 1])
+			if (array[j] > array[j + 1])
 			{
-				temp = arr[j];
-				arr[j] = arr[j + 1];
-				arr[j + 1] = temp;
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
 			}
 
 		}
