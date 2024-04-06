3a17f6a1d637ae4d4ed0e7205a2f910796020e2e
Davey <david.c.hays.iii@gmail.com>
2024-04-06 16:19:02 +0000

    added file 3

---
+++ 3-quick_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+ * quick_sort()- a very speedy sort!
+ * @array: the array to sort
+ * @size: the size of the array
+ *
+ * Return: no return
+ */
+
+void quick_sort(int *array, size_t size)
+{
+}
