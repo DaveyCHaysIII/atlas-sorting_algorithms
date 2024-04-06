bf1e7ea62df07cfdd5f62d5ef5a3964d36bf9628
Davey <david.c.hays.iii@gmail.com>
2024-04-06 17:00:40 +0000

    insertion sort test

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,7 +7,36 @@
  * Return: no return
  */
 
-void insertion_sort_list(listint_t **list);
+void insertion_sort_list(listint_t **list)
 {
+	listint_t *current, *temp;
 
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	current = *list;
+	current = current->next;
+	while (current)
+	{
+		while (current->prev && current->n < (current->prev)->n)
+		{
+			temp = current;
+			if (current->next)
+			{
+				(current->next)->prev = temp->prev;
+			}
+			(current->prev)->next = temp->next;
+			current = current->prev;
+			temp->prev = current->prev;
+			temp->next = current;
+			if (current->prev)
+				(current->prev)->next = temp;
+			current->prev = temp;
+			if (temp->prev == NULL)
+				*list = temp;
+			print_list(*list);
+			current = current->prev;
+		}
+		current = current->next;
+	}
 }
