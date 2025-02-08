79feadd9c94e4dced43e76f2689cc3c9401c1981
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2025-02-07 20:28:04 -0600

    push shell sort

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -10,5 +10,26 @@
 
 void shell_sort(int *array, size_t size)
 {
+	size_t gap, i, j;
+	int temp;
 
+	for (gap = 1; gap < size; gap = (gap * 3) + 1);
+
+	while (gap > 0)
+	{
+		for (i = gap; i < size; i++)
+		{
+			temp = array[i];
+			j = i;
+
+			while (j >= gap && array[j - gap] > temp)
+			{
+				array[j] = array[j - gap];
+				j -= gap;
+			}
+			array[j] = temp;
+		}
+		gap = (gap - 1) / 3;
+		print_array(array, size);
+	}
 }
