b3c97b686c6ce45206e1bf8fba6a5ed69bb41c6a
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2025-02-07 21:31:23 -0600

    changed some logic to better handle edge cases

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -10,7 +10,7 @@
 
 void counting_sort(int *array, size_t size)
 {
-	int max, i, j, count, *count_array, *output_array;
+	int max, i, *count_array, *output_array;
 
 	if (!array || !size || size == 1)
 		return;
@@ -25,21 +25,16 @@ void counting_sort(int *array, size_t size)
 
 	if (!count_array || !output_array)
 		return;
-	for (i = 0, count = 0; i < max + 1; i++)
-	{
-		for (j = 0; j < (int)size; j++)
-		{
-			if (array[j] == i)
-			count++;
-		}
-		count_array[i] = count;
-		count = 0;
-	}
+	for (i = 0; i < (int)size; i++)
+		count_array[array[i]]++;
 	for (i = 1; i < max + 1; i++)
-		count_array[i] = count_array[i - 1] + count_array[i];
+		count_array[i] += count_array[i - 1];
 	print_array(count_array, max + 1);
 	for (i = size - 1; i >= 0; i--)
+	{
 		output_array[count_array[array[i]] - 1] = array[i];
+		count_array[array[i]]--;
+	}
 	for (i = 0; i < (int)size; i++)
 		array[i] = output_array[i];
 	free(output_array);
