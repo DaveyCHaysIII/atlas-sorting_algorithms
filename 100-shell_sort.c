b1aa5b3f2e2d303a7862ad0608898a8784aaf21c
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2025-02-07 22:39:10 -0600

    small change to shell sort

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -13,7 +13,7 @@ void shell_sort(int *array, size_t size)
 	size_t gap, i, j;
 	int temp;
 
-	if (!array || !size || size == 0)
+	if (!array || !size || size == 1)
 		return;
 
 	for (gap = 1; gap < size / 3; gap = (gap * 3) + 1)
